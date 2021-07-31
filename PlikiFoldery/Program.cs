493b265d9459786cb01c9e9e42d64bcab3185231
kurs <kurs@DESKTOP-V3HDKPQ>
2021-07-31 14:32:57 +0200

    update

--- PlikiFoldery/Program.cs
+++ PlikiFoldery/Program.cs
@@ -11,6 +11,9 @@ namespace PlikiFoldery
     {
         static void Main(string[] args)
         {
+
+            CopyDir(@"c:\tmp", @"c:\tmp2\");
+
             String path = "C:/tmp/test.txt";
             if (File.Exists(path))
             {
@@ -37,6 +40,42 @@ namespace PlikiFoldery
             Directory.Delete("c:/tmp/csharp", true);
 
             Console.ReadKey();
+        }
+
+        static void CopyDir(string sourceDir, string targetDir)
+        {
+            if (!sourceDir.EndsWith(@"\"))
+            {
+                //sourceDir = sourceDir + @"\";
+                sourceDir += @"\";
+            }
+
+            if (!targetDir.EndsWith(@"\"))
+            {
+                targetDir += @"\";
+            }
+
+            // pobierz wszystkie pliki i podfoldery z sourceDir
+            string[] files = Directory.GetFileSystemEntries(sourceDir);
+
+            Directory.CreateDirectory(targetDir);
+            for (int i = 0; i < files.Length; i++)
+            {
+                Console.WriteLine(files[i]);
+                String srcFile = files[i];
+                FileAttributes attr = File.GetAttributes(srcFile);
+                if (attr == FileAttributes.Directory)
+                {
+                    // obsluga rekurencyjna dla katalogow
+                    String newFolder = targetDir + Path.GetFileName(srcFile);
+                    CopyDir(srcFile, newFolder);
+                } else
+                {
+                    // kopiowanie pliku
+                    String dstFile = targetDir + Path.GetFileName(srcFile);
+                    File.Copy(srcFile, dstFile, true);
+                }
+            }
         }
     }
 }
