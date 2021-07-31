ccf01936e4c8d2d0535668c7ceda7bdaaed68728
kurs <kurs@DESKTOP-V3HDKPQ>
2021-07-31 15:10:31 +0200

    update

---
+++ CoToZaPlik/Form1.Designer.cs
@@ -0,0 +1,97 @@
+﻿
+namespace CoToZaPlik
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lbInfo = new System.Windows.Forms.ListBox();
+            this.lFileName = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.ofd = new System.Windows.Forms.OpenFileDialog();
+            this.SuspendLayout();
+            // 
+            // lbInfo
+            // 
+            this.lbInfo.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.lbInfo.FormattingEnabled = true;
+            this.lbInfo.ItemHeight = 24;
+            this.lbInfo.Location = new System.Drawing.Point(12, 12);
+            this.lbInfo.Name = "lbInfo";
+            this.lbInfo.Size = new System.Drawing.Size(445, 316);
+            this.lbInfo.TabIndex = 0;
+            // 
+            // lFileName
+            // 
+            this.lFileName.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.lFileName.Location = new System.Drawing.Point(12, 346);
+            this.lFileName.Name = "lFileName";
+            this.lFileName.Size = new System.Drawing.Size(445, 23);
+            this.lFileName.TabIndex = 1;
+            this.lFileName.Text = "label1";
+            // 
+            // button1
+            // 
+            this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.button1.Location = new System.Drawing.Point(164, 399);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(116, 39);
+            this.button1.TabIndex = 2;
+            this.button1.Text = "Sprawdź plik";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // ofd
+            // 
+            this.ofd.Title = "Wybierz plik do sprawdzenia";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(469, 450);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.lFileName);
+            this.Controls.Add(this.lbInfo);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedDialog;
+            this.MaximizeBox = false;
+            this.Name = "Form1";
+            this.Text = "Co To Za Plik";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ListBox lbInfo;
+        private System.Windows.Forms.Label lFileName;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.OpenFileDialog ofd;
+    }
+}
+
---
+++ CoToZaPlik/Form1.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace CoToZaPlik
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            DialogResult result = ofd.ShowDialog();
+            if (result==DialogResult.OK)
+            {
+                lFileName.Text = ofd.FileName;
+
+                lFileName.Visible = true;
+                lbInfo.Items.Clear();
+
+                FileInfo fileInfo = new FileInfo(ofd.FileName);
+                lbInfo.Items.Add($"nazwa:{fileInfo.Name}");
+                lbInfo.Items.Add($"rozmiar:{fileInfo.Length}");
+                lbInfo.Items.Add($"folder:{fileInfo.DirectoryName}");
+                lbInfo.Items.Add($"roszerzenie:{fileInfo.Extension}");
+                lbInfo.Items.Add($"atrybuty:{fileInfo.Attributes}");
+                lbInfo.Items.Add($"czas kreacji:{fileInfo.CreationTime}");
+                lbInfo.Items.Add($"czas ostatniego dostepu:{fileInfo.LastAccessTime}");
+                lbInfo.Items.Add($"czas ostatniej modyf.:{fileInfo.LastWriteTime}");                    
+            }
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            lFileName.Visible = false;
+        }
+    }
+}
