6abeab1ee8e4b60d51c59acc8ad59cbfd8a17753
kurs <kurs@DESKTOP-V3HDKPQ>
2021-07-31 16:00:51 +0200

    update

---
+++ StooqParser/Form1.Designer.cs
@@ -0,0 +1,111 @@
+﻿
+namespace StooqParser
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.tbTicker = new System.Windows.Forms.TextBox();
+            this.dtStart = new System.Windows.Forms.DateTimePicker();
+            this.dtStop = new System.Windows.Forms.DateTimePicker();
+            this.btnGet = new System.Windows.Forms.Button();
+            this.dgvData = new System.Windows.Forms.DataGridView();
+            ((System.ComponentModel.ISupportInitialize)(this.dgvData)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // tbTicker
+            // 
+            this.tbTicker.Location = new System.Drawing.Point(22, 22);
+            this.tbTicker.Name = "tbTicker";
+            this.tbTicker.Size = new System.Drawing.Size(100, 20);
+            this.tbTicker.TabIndex = 0;
+            this.tbTicker.Text = "TSLA.US";
+            // 
+            // dtStart
+            // 
+            this.dtStart.Location = new System.Drawing.Point(149, 22);
+            this.dtStart.Name = "dtStart";
+            this.dtStart.Size = new System.Drawing.Size(200, 20);
+            this.dtStart.TabIndex = 1;
+            // 
+            // dtStop
+            // 
+            this.dtStop.Location = new System.Drawing.Point(370, 22);
+            this.dtStop.Name = "dtStop";
+            this.dtStop.Size = new System.Drawing.Size(200, 20);
+            this.dtStop.TabIndex = 2;
+            // 
+            // btnGet
+            // 
+            this.btnGet.Location = new System.Drawing.Point(589, 14);
+            this.btnGet.Name = "btnGet";
+            this.btnGet.Size = new System.Drawing.Size(107, 34);
+            this.btnGet.TabIndex = 3;
+            this.btnGet.Text = "Pobierz";
+            this.btnGet.UseVisualStyleBackColor = true;
+            this.btnGet.Click += new System.EventHandler(this.btnGet_Click);
+            // 
+            // dgvData
+            // 
+            this.dgvData.AllowUserToAddRows = false;
+            this.dgvData.AllowUserToDeleteRows = false;
+            this.dgvData.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dgvData.Location = new System.Drawing.Point(22, 70);
+            this.dgvData.Name = "dgvData";
+            this.dgvData.ReadOnly = true;
+            this.dgvData.Size = new System.Drawing.Size(914, 505);
+            this.dgvData.TabIndex = 4;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(970, 617);
+            this.Controls.Add(this.dgvData);
+            this.Controls.Add(this.btnGet);
+            this.Controls.Add(this.dtStop);
+            this.Controls.Add(this.dtStart);
+            this.Controls.Add(this.tbTicker);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dgvData)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox tbTicker;
+        private System.Windows.Forms.DateTimePicker dtStart;
+        private System.Windows.Forms.DateTimePicker dtStop;
+        private System.Windows.Forms.Button btnGet;
+        private System.Windows.Forms.DataGridView dgvData;
+    }
+}
+
---
+++ StooqParser/Form1.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace StooqParser
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            dtStop.Value = DateTime.Now;
+            dtStart.Value = DateTime.Now.AddMonths(-1);
+        }
+
+        private void btnGet_Click(object sender, EventArgs e)
+        {
+            String ticker = tbTicker.Text.Trim().ToLower();
+            String d1 = dtStart.Value.ToString("yyyyMMdd");
+            String d2 = dtStop.Value.ToString("yyyyMMdd");
+            String url = $"https://stooq.pl/q/d/l/?s={ticker}&d1={d1}&d2={d2}&i=d";
+            try
+            {
+                WebClient webClient = new WebClient();                
+                String s = webClient.DownloadString(url);
+                string[] lines = s.Split('\n');
+                for (int i = 1; i < lines.Length; i++)
+                {
+                    String line = lines[i];
+                }
+            }
+            catch (Exception exc)
+            {
+                MessageBox.Show(exc.Message);
+            }
+        }
+    }
+}
